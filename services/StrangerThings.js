b73af11d4bcf44db9c2dd5f7c51d41f9c0b2eb36
raffrasson <raf.frasson@gmail.com>
2022-03-29 03:26:17 -0300

    deploy dw - corrige var

--- services/StrangerThings.js
+++ services/StrangerThings.js
@@ -1,16 +1,18 @@
 'use strict';
+require('dotenv/config');
 
 const flipout = require('flipout');
+const { UPSIDEDOWN_MODE } = process.env;
 
 class StrangerThingsService {
   constructor(repository) {
     this.repository = repository;
   }
 
-  search({ page, size, ...params }, upsideDownMode) {
+  search({ page, size, ...params }, UPSIDEDOWN_MODE) {
     const characters = this.repository.search(params, { page, size });
 
-    if (upsideDownMode) {
+    if (UPSIDEDOWN_MODE) {
       return characters.map(({ name, origin, status }) => ({
         name: flipout(name),
         origin: flipout(origin),
